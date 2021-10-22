7df2c98e00bf7bb09bb2d9dcd04eb3d512dade4c
byc <buyicode@gmail.com>
2021-10-21 23:05:15 -0700

    add binary search problems

---
+++ Binary Search/Search for a Range.py
@@ -0,0 +1,34 @@
+# _*_ coding: utf-8 _*_
+"""
+# @Time : 10/21/2021 11:04 PM
+# @Author : byc
+# @File : Search for a Range.py
+# @Description :
+"""
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = -1, -1
+        l,r = 0, len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            if (m==l or nums[m-1]!=target) and nums[m]==target:
+                left = m
+                break
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        l,r = 0, len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            if (m==r or nums[m+1]!=target) and nums[m]==target:
+                right = m
+                break
+            if nums[m] > target:
+                r = m -1
+            else:
+                l = m + 1
+        return [left, right]
